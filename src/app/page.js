301666929aa12a70f9194adc740d544690d87bf6
MR-Titus02 <titusroxsan@gmail.com>
2026-05-09 11:40:01 +0530

    video recognition finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,87 @@
-import Image from "next/image";
+"use client";
+
+
+import { useRef, useEffect } from "react";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
 
 export default function Home() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runDetection = () => {
+    const doesExist = navigator.mediaDevices.getUserMedia({ video: true }) instanceof Promise;
+    if (!doesExist) {
+      console.log("Camera is supported"); 
+    };
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const webcamPromise = navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } }).then((stream) => {
+          videoRef.current.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+
+        const modelPromise = cocoSsd.load();
+
+        Promise.all([modelPromise, webcamPromise]).then((values) => {
+          const model = values[0];
+          detectFrame(videoRef.current, model);
+        });
+
+        }
+      }
+
+  const detectFrame = (video, model) => {
+    model.detect(video).then((predictions) => {
+      renderPredictions(predictions);
+      requestAnimationFrame(() => {        detectFrame(video, model);
+      });
+    });
+  };
+
+  const renderPredictions = (predictions) => {
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    
+    predictions.forEach((prediction) => {
+      const [x, y, width, height] = prediction.bbox;
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      ctx.fillRect(x, y, textWidth + 4, parseInt(font, 10) + 4);
+      
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+  
+    });
+  };
+
+  useEffect(() => {
+    runDetection();
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="relative h-screen flex items-center justify-center bg-indigo-400">
+      <video autoPlay ref={videoRef} 
+      className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 border-8 border-dashed rounded-xl"
+      />
+
+      <canvas ref={canvasRef}
+        className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        width={500} 
+        height={500}
+      />
     </div>
   );
 }
